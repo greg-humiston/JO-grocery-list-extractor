5b575d7f848456e30451a5361740bd99407924a7
Greg Humistion <antihero989@gmail.com>
2025-01-07 10:51:22 -0600

    progress commit

--- pages/content-runtime/src/index.ts
+++ pages/content-runtime/src/index.ts
@@ -2,3 +2,22 @@ import { mount } from '@src/Root';
 
 mount();
 console.log('runtime script loaded');
+
+const retrieveGroceryListFromElement = (element: Element) => {
+  const miniCartItems = element.getElementsByTagName('mini-cart-item') || [];
+  const groceryList = Array.from(miniCartItems).map(miniCartItem => {
+    const itemTitle = miniCartItem.getElementsByClassName('cart-dst-product-name')?.[0].textContent || '';
+    const itemQuantity = miniCartItem.getElementsByClassName('cart-dst-quantity-text')?.[0].textContent || 0;
+
+    return {
+      item: itemTitle,
+      quantity: itemQuantity,
+    };
+  });
+
+  console.log(groceryList);
+};
+
+const targetElements = document.getElementsByClassName('cart-dst-scroll-store');
+const targetElement = targetElements?.[0];
+retrieveGroceryListFromElement(targetElement);
--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -3,3 +3,15 @@ import { toggleTheme } from '@src/toggleTheme';
 console.log('content script loaded');
 
 void toggleTheme();
+
+const addClass = (elements: HTMLCollectionOf<Element>, newContent: string) => {
+  Array.from(elements).forEach(element => {
+    const div = document.createElement('div');
+    div.textContent = newContent;
+    div.className = 'injected-content';
+    element.appendChild(div);
+  });
+};
+
+const targetElements = document.getElementsByClassName('cart-dst-scroll-store');
+addClass(targetElements, 'Additional helpful information');
